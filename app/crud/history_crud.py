42890d24658b7a314f02a61c9180a1945fdf33e9
Abdelaziz-LAISSAOUI <azizaissa2004@gmail.com>
2024-06-01 17:26:15 +0100

    adjust history schemas and add get_user_history

--- app/crud/history_crud.py
+++ app/crud/history_crud.py
@@ -1,10 +1,14 @@
 from sqlalchemy.orm import Session
 from ..models import History 
 from ..schemas.history_schemas import HistoryCreate
-
+import uuid
 def create_history(db: Session, history: HistoryCreate):
     db_history = History(**history.model_dump())
     db.add(db_history)
     db.commit()
     db.refresh(db_history)
     return db_history
+
+
+def get_user_history(db: Session, user_id: uuid.UUID, lesson_name: str):
+    return db.query(History).filter(History.user_id == user_id, History.lesson_name == lesson_name).all()
--- app/schemas/history_schemas.py
+++ app/schemas/history_schemas.py
@@ -3,11 +3,13 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
-class HistoryBase(BaseModel):
+class HistoryBase(BaseModel): # add difficulty
     user_id: uuid.UUID
     question_id: uuid.UUID
     user_answer: str
     feedback: str
+    lesson_name : str
+    type : str
 
 class HistoryCreate(HistoryBase):
     pass
